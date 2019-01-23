fde86abc918ca5c2121445eec70231e340b294b4
Joe McColl <joe4kool@gmail.com>
2019-01-23 14:27:29 +0000

    lifegoal.js model working, need to connect to server to allow http post request and get

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,5 @@
 const LifeGoalsFormView = require('./views/life_goals_form_view.js')
+const LifeGoals = require('./models/life_goals.js')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log("Javascript loaded");
@@ -6,4 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const lifeGoalsForm = document.querySelector('form#goals-form');
   const lifeGoalsFormView = new LifeGoalsFormView(lifeGoalsForm);
   lifeGoalsFormView.bindEvents();
+
+  const lifeGoals = new LifeGoals();
+  lifeGoals.bindEvents();
+  lifeGoals.getData();
 });
---
+++ client/src/models/life_goals.js
@@ -0,0 +1,33 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const LifeGoals = function (url) {
+  this.url = 'http://localhost:3000/api/goalslist';
+  this.request = new RequestHelper(this.url);
+};
+
+LifeGoals.prototype.bindEvents = function () {
+  PubSub.subscribe("LifeGoalsFormView:goal-submitted", (event) => {
+    this.postGoals(event.detail);
+  })
+
+};
+
+LifeGoals.prototype.getData = function () {
+  this.request.get()
+  .then((goals) => {
+    PubSub.publish("LifeGoals:data-loaded", goals);
+  })
+  .catch(console.error);
+};
+
+LifeGoals.prototype.postGoals = function (goal) {
+  console.log(goal);
+  this.request.post(goal)
+  .then((goals) => {
+    PubSub.publish("LifeGoals:data-loaded", goals);
+  })
+  .catch(console.error);
+};
+
+module.exports = LifeGoals;
